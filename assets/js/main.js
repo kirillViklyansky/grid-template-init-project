41340549e23c115b3cc8df14a11bcfdbebc52ab4
Gladunenko Nikita <gnp.glad@gmail.com>
2020-03-22 17:47:00 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function () {
+
+
+
+  $('.header-navbar__hamburger').on('click', function () {
+
+    $(this).toggleClass('active');
+
+    $('.header-navbar__list').toggleClass('active');
+
+  });
+
+
+
+});
